5ef43b44a7e4f413c4fa4e2fae03e2d8e9b09281
Conner D <connerdunn7399@gmail.com>
2014-11-07 18:46:31 -0700

    I think i'm going to try my own algorithm instead!

--- Solve.cpp
+++ Solve.cpp
@@ -1,23 +1,11 @@
-// source: http://en.wikipedia.org/wiki/Sudoku_solving_algorithms
-//  Initialize 2D array with 81 empty grids (nx = 9, ny = 9)
-//  Fill in some empty grid with the known values
-//  Make an original copy of the array
-//  Start from top left grid (nx = 0, ny = 0), check if grid is empty
-//  if (grid is empty) {
-//    assign the empty grid with values (i)
-//    if (no numbers exists in same rows & same columns same as (i) & 3x3 zone (i) is currently in)
-//      fill in the number
-//    if (numbers exists in same rows & same columns same as (i) & 3x3 zone (i) is currently in)
-//      discard (i) and repick other values (i++)
-//  }
-//  else {
-//  while (nx < 9) {
-//    Proceed to next row grid(nx++, ny)
-//      if (nx equals 9) {
-//        reset nx = 1
-//        proceed to next column grid(nx,ny++)
-//          if (ny equals 9) {
-//            print solution
-//      }
-//    }
-//  }
+/*
+Create board (9, 9) w/ known values
+Copy the array to store known values
+Iterate through all 81 positions starting with the top left one
+  base case: all 81 pos have a number, return true
+
+  for each pos find the valid vaules (rows, cols, and 3x3 square)
+  if there are no valid numbers return false
+  try a valid one and call the function again
+
+*/
