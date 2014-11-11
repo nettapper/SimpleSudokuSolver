25c010f61f38b6abb73cae8c9e06c18ef0a6e584
Conner D <connerdunn7399@gmail.com>
2014-11-10 19:51:50 -0700

    *NotWorking* First try at a solution

--- Solve.cpp
+++ Solve.cpp
@@ -12,7 +12,7 @@ Iterate through all 81 positions starting with the top left one
 #include <stdio.h>
 
 void printBoard(int board[9][9]);
-bool solve(int board[9][9]);
+bool solve(int (*board)[9][9]);
 
 int main(){
   int board[9][9] = {{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -25,7 +25,7 @@ int main(){
 		      {0, 0, 0, 0, 0, 0, 0, 0, 0},
 		      {0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-  if(solve(board)){
+  if(solve(&board)){
     printBoard(board);
   } else {
     printf("Board couldn't be solved\n");
@@ -49,7 +49,47 @@ void printBoard(int board[9][9]){
   }
 }
 
-bool solve(int board[9][9]){
-  // do stuff
+bool solve(int (*board)[9][9]){
+  // todo: should i keep a count to know when im done?
+  for(int i = 0; i < 9; i++){
+    for(int j = 0; j < 9; j++){
+      // currently iterating through all values
+      if((*board)[i][j] == 0){  // needs a value
+	
+	// find un-used values 
+	int notUsed[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};  // bool val for used or not
+	for(int k = 0; k < 9; k++){
+	  if((*board)[i][k] == 0) {  // look at cols
+	    notUsed[k] = 1;
+	  }
+	  if((*board)[k][j] == 0){  // look at rows
+	    notUsed[k] = 1;
+	  }
+	}
+	int a = i - (i%3); // left of 3x3 square (row)
+	int b = j - (j%3); // top of 3x3 square (col)
+	for(int k = a; k < a+3; k++){
+	  for(int l = b; l < b+3; l++){
+	    if((*board)[k][l] == 0){  // look at 3x3
+	      notUsed[k] = 1;
+	    }
+	  }
+	}
+	for(int k = 0; k < 9; k++){  // try out non-used values
+	  if(notUsed[k] == 0){  // not valid value
+	    continue;
+	  }
+	  (*board)[i][j] = k+1;
+	  if(i == 9 && j == 9){  // base case (last num found)
+	    return true;
+	  }
+	  if(solve(board)){  // sol found!
+	    return true;
+	  }
+	}
+      }
+    }
+  }
+  printBoard(*board);
   return false;
 }
