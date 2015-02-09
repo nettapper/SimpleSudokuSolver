e8df3058af36ffde91cde0c1e508c119dbd49724
Conner D <connerdunn7399@gmail.com>
2015-02-08 23:11:04 -0700

    maybe working... very very slow

--- solver.py
+++ solver.py
@@ -15,69 +15,27 @@ for i in range(9):
     assert(len(board[i]) == 9)  # num of cols needs to be 9 for each row
 
 
-print("Div and Conq")
+print("Div and Conq:")
 
-def solve(board, x=0, y=0):
-    # debugging
-    printBoard(board)
-    print("***")
-    # end debugging
-    assert(board[x][y] == 0)  # must be an unkown val to start
-    # sol has 81 squares that are not 0's
-    isSoln = True
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            if(board[i][j] == 0):
-                isSoln = False  # a val was not found!
-                break
-            for n in range(1,10):
-                if(isValid(board, i, j, n)):
-                    isSoln = False  # shouldn't have any valid pos left
-    if(isSoln): return True
-    # find possible vals for board[x,y]
-    possible = getPossible(board, x, y)
-    if(len(possible) == 0): return False
-    for x in range(len(possible)):
-        board[x][y] = possible[x]
-        # then inc the x or y vals to a non-zero
-        i, j = x, y
-        while(board[i][j] != 0):
-            j += 1
-            if(j >= len(board[i])):
-                i += 1
-                j %= len(board[i])
-        solve(board, i, j)
+def solve(board):
+    # get our x and y position
+    x,y = getNext(board)
+    if(x == -1 or y == -1):
+        return True  # no more empty positions
+
+    for n in range(1,10):
+        if(isValid(board, x, y, n)):
+            board[x][y] = n  # try n
+            solve(board)
+            board[x][y] = 0  # n didn't work try next
     return False
 
-def getPossible(board, i, j):
-    vals = [1,2,3,4,5,6,7,8,9]
-    # check row
-    for x in range(len(board[i])):
-        current = board[i][x]
-        try:
-            vals.remove(current)
-        except ValueError:
-            pass
-    # check col
+def getNext(board):
     for x in range(len(board)):
-        current = board[x][j]
-        try:
-            vals.remove(current)
-        except ValueError:
-            pass
-    # check 3x3 box
-    a = i // 3
-    b = j // 3
-    for x in range(a*3, (a+1)*3):
-        for y in range(b*3, (b+1)*3):
-            current = board[x][y]
-            # print("c ", current, " | x: ", x, " y: ", y)
-            try:
-                vals.remove(current)
-            except ValueError:
-                pass
-    # return what is left of the array
-    return vals
+        for y in range(len(board[0])):
+            if(board[x][y] == 0):
+                return x,y
+    return -1,-1
 
 def isValid(board, i, j, n):
     # check row
