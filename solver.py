ea7cd924dbeb4fdb25765dad097839d2d0ce8cdd
Conner D <connerdunn7399@gmail.com>
2015-02-14 18:09:53 -0700

    cleaned up the code and factored into main

--- solver.py
+++ solver.py
@@ -1,45 +1,43 @@
-print("Starting Up...")
 
-boarda = [[7,9,0, 0,0,0, 3,0,0],
-    [0,0,0, 0,0,6, 9,0,0],
-    [8,0,0, 0,3,0, 0,7,6],
-    [0,0,0, 0,0,5, 0,0,2],
-    [0,0,5, 4,1,8, 7,0,0],
-    [4,0,0, 7,0,0, 0,0,0],
-    [6,1,0, 0,9,0, 0,0,8],
-    [0,0,2, 3,0,0, 0,0,0],
-    [0,0,9, 0,0,0, 0,5,4]]
+def main():
+    print("Starting Up...")
+    board = [  # spacing should make the board more readable
+        [7,9,0, 0,0,0, 3,0,0],
+        [0,0,0, 0,0,6, 9,0,0],
+        [8,0,0, 0,3,0, 0,7,6],
 
-# testing with this empty board to make sure the getNext returns correctly
-board = [[0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0],
-    [0,0,0, 0,0,0, 0,0,0]]
+        [0,0,0, 0,0,5, 0,0,2],
+        [0,0,5, 4,1,8, 7,0,0],
+        [4,0,0, 7,0,0, 0,0,0],
 
-assert(len(board) == 9)  # num of rows must be 9
-for i in range(9):
-    assert(len(board[i]) == 9)  # num of cols needs to be 9 for each row
+        [6,1,0, 0,9,0, 0,0,8],
+        [0,0,2, 3,0,0, 0,0,0],
+        [0,0,9, 0,0,0, 0,5,4]]
 
-
-print("Div and Conq:")
+    print("Original Board:")
+    printBoard(board)
+    print("")
+    print("Div and Conq")
+    solve(board)
+    print("")
+    print("Solved Board:")
+    printBoard(board)
 
 def solve(board):
-    # printBoard(board)  # debugging
-    # print("")  # debugging
-    # get our x and y position
+    assert(len(board) == 9)  # num of rows must be 9
+    for i in range(9):
+        assert(len(board[i]) == 9)  # num of cols needs to be 9 for each row
+
+    solveBackTrack(board)
+
+def solveBackTrack(board):
     x,y = getNext(board)
     if(x == -1 or y == -1):
         return True  # no more empty positions
-
     for n in range(1,10):
         if(isValid(board, x, y, n)):
             board[x][y] = n  # try n
-            if(solve(board)):
+            if(solveBackTrack(board)):
                 return True
             board[x][y] = 0  # n didn't work try next
     return False
@@ -73,10 +71,6 @@ def printBoard(board):
     for i in range(len(board)):
         print(board[i])
 
-print("Original Board:")
-printBoard(board)
-print("")
-solve(board)
-print("")
-print("Solved Board:")
-printBoard(board)
+
+# run the program!
+main()
