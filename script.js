6aef6adc9944c07f95c5eeccfcf855b14446e2e3
Conner D <connerdunn7399@gmail.com>
2015-02-16 17:11:46 -0700

    remove old lines of jquery, index.html says hi

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// My script
-// short hand for $(document).ready(function(){
-$(function(){
-    $("a").addClass("aBold");
-    $("a").click(function(event){
-	// alert("baby come back!");
-	$(this).hide(1000);
-	event.preventDefault();
-	console.log("i won't let you leave!!");
-    });
-});
+// Sudoku Solver in JS
