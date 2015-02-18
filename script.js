b546c1beb86bdbc793ee2ffef2c5428615121245
Conner D <connerdunn7399@gmail.com>
2015-02-18 12:42:25 -0700

    isValid method done and testing with almost finished board

--- script.js
+++ script.js
@@ -22,6 +22,16 @@ var board = [[7,9,0, 0,0,0, 3,0,0],
      [0,0,2, 3,0,0, 0,0,0],
      [0,0,9, 0,0,0, 0,5,4]];
 
+var almost = [[0,1,7, 3,6,9, 8,2,5],  // missing one should be a 4
+     [6,3,2, 1,5,8, 9,4,7],
+     [9,5,8, 7,2,4, 3,1,6],
+     [8,2,5, 4,3,7, 1,6,9],
+     [7,9,1, 5,8,6, 4,3,2],
+     [3,4,6, 9,1,2, 7,5,8],
+     [2,8,9, 6,4,3, 5,7,1],
+     [5,7,3, 2,9,1, 6,8,4],
+     [1,6,4, 8,7,5, 2,9,3]];
+
 // def stringParse(string):
 //     assert(len(string) == 9*9)
 //     board = []
@@ -50,7 +60,7 @@ function solve(board) {
 }
 
 function solveBackTrack(board){
-    next = getNext(board);
+    var next = getNext(board);
     if(next == null){
 	return true;  // no more empty positions
     }
@@ -84,23 +94,34 @@ function getNext(board){
     return null;
 }
 
-// def isValid(board, i, j, n):
-//     # check row
-//     for x in range(len(board[i])):
-//         current = board[i][x]
-//         if (current == n): return False
-//     # check col
-//     for x in range(len(board)):
-//         current = board[x][j]
-//         if (current == n): return False
-//     # check 3x3 box
-//     a = i // 3
-//     b = j // 3
-//     for x in range(a*3, (a+1)*3):
-//         for y in range(b*3, (b+1)*3):
-//             current = board[x][y]
-//             if (current == n): return False
-//     return True
+function isValid(board, i, j, n){
+    // check row
+    for(x=0; x<board[i].length; x++){
+	var current = board[i][x];
+	if(current == n){
+	    return false;
+	}
+    }
+    // check col
+    for(y=0; y<board.length; y++){
+	var current = board[y][j];
+	if(current == n){
+	    return false;
+	}
+    }
+    // 3x3 box
+    var a = Math.floor(i / 3);  // might need to round
+    var b = Math.floor(j / 3);
+    for(x=a*3; x<(a+1)*3; x++){
+	for(y=b*3; y<(b+1)*3; y++){
+	    var current = board[x][y];
+	    if(current == n){
+		return false;
+	    }
+	}
+    }
+    return true;
+}
 
 // def printBoard(board):
 //     for i in range(len(board)):
